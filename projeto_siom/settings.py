130ee9634c1557ae9c8770c37f66e05fb9091ab3
petual1 <danielalexandre202@hotmail.com>
2023-11-27 02:57:55 -0300

    requirements changes

--- projeto_siom/settings.py
+++ projeto_siom/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_siom'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://siomweb.scm.azurewebsites.net",
+]
+
+CORS_ALLOW_HEADERS = [
+    'access-control-allow-origin',
+    'content-type',
 ]
 
 ROOT_URLCONF = 'projeto_siom.urls'
